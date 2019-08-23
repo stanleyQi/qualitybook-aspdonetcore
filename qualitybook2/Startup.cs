fa8752c7a88d7de7b49e470f64d8a6964627a97e
stanleyQi <qili811227@gmail.com>
2019-08-24 00:18:13 +0800

    modify startup.cs

--- qualitybook2/Startup.cs
+++ qualitybook2/Startup.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;  
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
